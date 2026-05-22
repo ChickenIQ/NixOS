5dfe52120549e136bd6cdc44c616e80421a19b7c
ChickenIQ <emibenzar@gmail.com>
2026-05-23 02:42:45 +0300

    cleanup

--- modules/home/gpu-screen-recorder.nix
+++ modules/home/gpu-screen-recorder.nix
@@ -1,28 +1,24 @@
 { config, pkgs, ... }:
-
-let
-  replayDir = "${config.home.homeDirectory}/Videos/Replays";
-
-  saveReplay = pkgs.writeShellApplication {
-    name = "gpu-screen-recorder-save-replay";
-
-    runtimeInputs = with pkgs; [
-      libnotify
-      procps
-    ];
-
-    text = ''
-      if pkill -SIGUSR1 -f '/bin/.wrapped/gpu-screen-recorder'; then
-        notify-send "GPU Screen Recorder" "Replay saved!" --urgency=low
-      else
-        notify-send "GPU Screen Recorder" "Failed to save replay!" --urgency=critical
-        exit 1
-      fi
-    '';
-  };
-in
 {
-  home.packages = [ saveReplay ];
+  home.packages = [
+    (pkgs.writeShellApplication {
+      name = "gpu-screen-recorder-save-replay";
+
+      runtimeInputs = with pkgs; [
+        libnotify
+        procps
+      ];
+
+      text = ''
+        if pkill -SIGUSR1 -f '/bin/.wrapped/gpu-screen-recorder'; then
+          notify-send "GPU Screen Recorder" "Replay saved!"
+        else
+          notify-send "GPU Screen Recorder" "Failed to save replay!"
+          exit 1
+        fi
+      '';
+    })
+  ];
 
   systemd.user.services.gpu-screen-recorder-replay = {
     Install.WantedBy = [ "graphical-session.target" ];
@@ -30,24 +26,17 @@ in
     Unit = {
       Description = "GPU Screen Recorder replay buffer";
       PartOf = [ "graphical-session.target" ];
-      After = [
-        "graphical-session.target"
-        "pipewire.service"
-      ];
+      After = [ "pipewire.service" ];
     };
 
     Service = {
-      ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p ${replayDir}";
       Restart = "on-failure";
-      RestartSec = "3s";
+      RestartSec = "5s";
       ExecStart = ''
         ${pkgs.bash}/bin/sh -c '${pkgs.gpu-screen-recorder}/bin/gpu-screen-recorder \
-          -w screen \
-          -r 60 \
-          -c mp4 \
-          -k av1 \
           -a "$(${pkgs.pulseaudio}/bin/pactl get-default-sink).monitor|rnnoise_source" \
-          -o ${replayDir}'
+          -o ${config.home.homeDirectory}/Videos/Replays \
+          -w screen -r 60 -c mp4 -k av1'
       '';
     };
   };
