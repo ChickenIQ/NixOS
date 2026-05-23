b801d22253ec07889988f70fea0e717a4d1214b4
ChickenIQ <emibenzar@gmail.com>
2026-05-23 20:56:02 +0300

    refactor replays

--- modules/home/replays.nix
+++ modules/home/replays.nix
@@ -1,32 +1,46 @@
 { config, pkgs, ... }:
+let
+  replayNotify = pkgs.writeShellApplication {
+    name = "replay-notify";
+
+    runtimeInputs = with pkgs; [
+      libnotify
+      sox
+    ];
+
+    text = ''
+      notify-send -e "GPU Screen Recorder" "Replay saved successfully!"
+      play -qn synth .07 sine C4 fade h .004 .07 .03 gain -16
+      play -qn synth .1 sine G4 fade h .004 .1 .045 gain -17
+    '';
+  };
+
+  saveReplay = pkgs.writeShellApplication {
+    name = "gpu-screen-recorder-save-replay";
+
+    runtimeInputs = with pkgs; [
+      libnotify
+      systemd
+      sox
+    ];
+
+    text = ''
+      if ! systemctl --user kill -s SIGUSR1 --kill-who=main gpu-screen-recorder-replay; then
+        notify-send -e "GPU Screen Recorder" "Failed to save replay!"
+        play -qn synth .065 sine E4 fade h .005 .065 .028 gain -18
+        play -qn synth .105 sine F4 fade h .005 .105 .048 gain -18 
+        exit 1
+      fi
+    '';
+  };
+in
 {
-  home.packages = [
-    (pkgs.writeShellApplication {
-      name = "gpu-screen-recorder-save-replay";
-
-      runtimeInputs = with pkgs; [
-        libnotify
-        procps
-        sox
-      ];
-
-      text = ''
-        if pkill -SIGUSR1 -f '/bin/.wrapped/gpu-screen-recorder'; then
-          (play -q -n synth 0.07 sine C4 fade h 0.004 0.07 0.03 gain -16; sleep 0.025; play -q -n synth 0.10 sine G4 fade h 0.004 0.10 0.045 gain -17) >/dev/null 2>&1 &
-          notify-send "GPU Screen Recorder" "Replay saved!"
-        else
-          (play -q -n synth 0.065 sine E4 fade h 0.005 0.065 0.028 gain -18; sleep 0.024; play -q -n synth 0.105 sine F4 fade h 0.005 0.105 0.048 gain -18) >/dev/null 2>&1 &
-          notify-send "GPU Screen Recorder" "Failed to save replay!"
-          exit 1
-        fi
-      '';
-    })
-  ];
+  home.packages = [ saveReplay ];
 
   systemd.user.services.gpu-screen-recorder-replay = {
     Install.WantedBy = [ "graphical-session.target" ];
     Unit = {
-      Description = "GPU Screen Recorder replay buffer";
+      Description = "GPU Screen Recorder Replay Service";
       PartOf = [ "graphical-session.target" ];
       After = [ "pipewire.service" ];
     };
@@ -35,12 +49,13 @@
       Restart = "on-failure";
       RestartSec = "5s";
       ExecStart = ''
-        ${pkgs.bash}/bin/sh -c '${pkgs.gpu-screen-recorder}/bin/gpu-screen-recorder \
+        ${pkgs.gpu-screen-recorder}/bin/gpu-screen-recorder \
           -w screen -r 60 -c mp4 -k av1 -fm cfr -bm cbr -q 40000 \
           -o ${config.home.homeDirectory}/Videos/Replays \
+          -sc ${replayNotify}/bin/replay-notify \
           -a "default_output|rnnoise_source" \
           -a "default_output" \
-          -a "rnnoise_source"'
+          -a "rnnoise_source"
       '';
     };
   };
