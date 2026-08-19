4a9e8845489d4d9824dd97db3a28913577300a0d
ChickenIQ <emibenzar@gmail.com>
2026-08-19 12:13:28 +0300

    my oniri patch is now upstream

--- modules/pkgs/oniri.nix
+++ modules/pkgs/oniri.nix
@@ -4,17 +4,15 @@
     {
       packages.oniri = pkgs.rustPlatform.buildRustPackage rec {
         pname = "oniri";
-        version = "1.3.3";
-        cargoHash = "sha256-mO0Q8Q2dTBhGCKQlhHSFzz8YHpH/MtYbCuH4PIVZPpw=";
+        version = "1.3.4";
+        cargoHash = "sha256-aoqtaLKGflXyg+l3f1MYSOqLJILhnY8gS8ttC0wB7iw=";
 
         src = pkgs.fetchFromGitHub {
           repo = "oniri";
           owner = "Antiz96";
           tag = "v${version}";
-          hash = "sha256-/rTE4EQ2GJlfeudWnM9Qh9lpDnGXM86Ilmquts6BHJM=";
+          hash = "sha256-kJTE873WwM9E8SCedACCDjTWJ4sdDsFOuIQw2KLHK5s=";
         };
-
-        patches = [ ./oniri.patch ];
 
         meta = {
           description = "Automatically maximize the only window of a niri workspace";
