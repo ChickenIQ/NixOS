9761c2695a16376d5065e6e17b638d443287e0f3
ChickenIQ <emibenzar@gmail.com>
2026-09-25 04:14:16 +0300

    add installer

---
+++ modules/flake/parts/tools.nix
@@ -0,0 +1,52 @@
+{
+  perSystem =
+    {
+      inputs',
+      pkgs,
+      lib,
+      ...
+    }:
+    let
+      disko = pkgs.writeShellApplication {
+        runtimeInputs = [ inputs'.disko.packages.disko ];
+        name = "disko";
+
+        text = ''
+          action="$1"; host="$2"; disk="''${3:-}"
+
+          [ -b "$disk" ] || exit 1
+          ln -sf "$disk" /dev/diskoTarget
+
+          case "$action" in
+            format) mode="destroy,format,mount" ;;
+            mount) mode="mount" ;;
+            *) exit 1 ;;
+          esac
+
+          disko --mode "$mode" --flake ".#$host"
+        '';
+      };
+
+      installer = pkgs.writeShellApplication {
+        name = "installer";
+
+        runtimeInputs = [
+          pkgs.nixos-install-tools
+          disko
+        ];
+
+        text = ''
+          host="$1" disk="''${2:-}"
+
+          [ -z "$disk" ] || disko format "$host" "$disk"
+          nixos-install --no-root-passwd --no-channel-copy --flake ".#$host"
+        '';
+      };
+    in
+    {
+      apps = {
+        installer.program = lib.getExe installer;
+        disko.program = lib.getExe disko;
+      };
+    };
+}
--- modules/home/helium.nix
+++ modules/home/helium.nix
@@ -2,13 +2,21 @@
   flake.homeModules.helium =
     { pkgs, ... }:
     {
-      xdg.mimeApps.defaultApplications = {
-        "x-scheme-handler/https" = "helium.desktop";
-        "x-scheme-handler/http" = "helium.desktop";
-        "application/xhtml+xml" = "helium.desktop";
-        "text/html" = "helium.desktop";
-      };
-
       home.packages = [ pkgs.self.helium ];
+
+      xdg = {
+        mimeApps.defaultApplications = {
+          "x-scheme-handler/https" = "helium.desktop";
+          "x-scheme-handler/http" = "helium.desktop";
+          "application/xhtml+xml" = "helium.desktop";
+          "text/html" = "helium.desktop";
+        };
+
+        desktopEntries.chatgpt = {
+          exec = ''helium --app="https://chatgpt.com/?temporary-chat=true"'';
+          name = "ChatGPT";
+          icon = "helium";
+        };
+      };
     };
 }
